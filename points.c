5cdc07bba74d6855e40af0a94c48dde0edd60bd3
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2022-08-30 21:08:32 +0000

    Tue, Aug 30, 2022, 9:08 PM +00:00

--- points.c
+++ points.c
@@ -4,13 +4,14 @@
 int main (void )
 
 {
+    int z=2;
     int points = get_int("How many points did you lose?\n");
 
-    if (points < 2)
+    if (points < z)
     {
         printf("You lost fewer points than me\n");
     }
-    else if (points >2)
+    else if (points >z)
     {
         printf("you lost more points than me\n");
     }
