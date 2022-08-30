e6bbb361bf6368bd76dc9c3b409bcd83cb87d4b8
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2022-08-30 21:26:20 +0000

    Tue, Aug 30, 2022, 9:26 PM +00:00

--- agree.c
+++ agree.c
@@ -1,1 +1,22 @@
-copy
+#include <cs50.h>
+#include <stdio.h>
+
+int main (void)
+{
+    //Prompt user to agree
+
+    char c = get char ("Do you Agree?\n");
+
+    //check whether user agreed
+
+    if (c  == y)
+    {
+        printf("Agreed.\n");
+    }
+    else if (c == n)
+    {
+        printf("Disagree.\n");
+    }
+
+
+}
