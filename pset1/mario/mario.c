0faa5ab81b0134d7ce2e07ac670349c5c96bc060
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2022-09-12 22:43:06 +0000

    Mon, Sep 12, 2022, 10:43 PM +00:00

--- pset1/mario/mario.c
+++ pset1/mario/mario.c
@@ -11,11 +11,11 @@ int main(void)
     }
     while (n < 1);
 
-    for (int i = 0, i < n, i++);
+    for (int i = 0; i < n; i++)
     {
         printf("#");
     }
-        printf("\n")
+        printf("\n");
 }
 
 
