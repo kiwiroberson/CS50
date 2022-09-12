bd20e0719d6125c25c847c27e11fea561a69dd31
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2022-09-12 22:48:41 +0000

    Mon, Sep 12, 2022, 10:48 PM +00:00

--- pset1/mario/mario.c
+++ pset1/mario/mario.c
@@ -11,11 +11,14 @@ int main(void)
     }
     while (n < 1);
 
+    //For each row
     for (int i = 0; i < n; i++)
     {
-        printf("#");
+         //for each collumn
+        for (int j = 0; j < n; j++)
+            //Print a Brick
+            printf("#");
     }
-        printf("\n");
-}
-
-
+        //Move to next row
+        "printf("\n");
+        
