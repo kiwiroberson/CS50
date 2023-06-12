ccf6f871e4e04d76fbbe55c3ebe0d6c0ed66bcaa
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-06-12 03:20:23 +0000

    Mon, Jun 12, 2023, 4:20 AM +01:00

--- pset1/mario.c
+++ pset1/mario.c
@@ -3,10 +3,10 @@
 
 int main (void)
 {
-    int s = 6;
-    for (int i = 0; i < s; i++)
+    const int n = 3;
+    for (int i = 0; i < n; i++)
     {
-       for (int j = 0; j <s; j++)
+       for (int j = 0; j < n; j++)
         {
             printf("#");
         }
