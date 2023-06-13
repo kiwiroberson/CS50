7fe381636f9f9141206eadc0765014203b31257b
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-06-13 01:59:52 +0000

    Tue, Jun 13, 2023, 2:59 AM +01:00

--- pset1/population.c
+++ pset1/population.c
@@ -8,12 +8,12 @@ int main (void)
 // ask for starting population end ending population
 
     int i = get_int("Starting Population? ");
-    int j = get_int ("End Population> ");
+    int j = get_int ("End Population? ");
     int c = 0;
     int n=0;
 //run loop while n < end population
 
-    while (n<j)
+    while ( n < j)
     {
        //loop for: n gets n + n/3 - n/4, then add one to counter
         n = i;
