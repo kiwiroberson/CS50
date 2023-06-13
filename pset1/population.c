74902d7f70708931fb00bf7f34ae6e38e7461c68
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-06-13 01:56:12 +0000

    Tue, Jun 13, 2023, 2:56 AM +01:00

--- pset1/population.c
+++ pset1/population.c
@@ -9,13 +9,18 @@ int main (void)
 
     int i = get_int("Starting Population? ");
     int j = get_int ("End Population> ");
-
+    int c = 0
 
 //run loop while n < end population
 
-
-
-    //loop for: n gets n + n/3 - n/4, then add one to counter
+    while (n<j)
+    {
+       //loop for: n gets n + n/3 - n/4, then add one to counter
+        n = i;
+        n = (n + n/3 - n/4);
+        c++;
+    }
+    printf("Years: )
 
 //print  counter as output
 
