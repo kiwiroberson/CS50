aa5dbc8fc27d582859806e9669a1fe74c7e48914
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-06-13 01:42:31 +0000

    Tue, Jun 13, 2023, 2:42 AM +01:00

--- pset1/population.c
+++ pset1/population.c
@@ -1,3 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
 
+lklejk void
+
+// ask for starting population end ending population
+
+//run loop while n < end population
+
+    //loop for: n gets n + n/3 - n/4, then add one to counter
+
+//print  counter as output
