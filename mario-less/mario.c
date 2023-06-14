ccb1c61bfc5d40d460a204dd8bcca09ba9f10518
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-06-14 22:07:29 +0000

    Wed, Jun 14, 2023, 11:07 PM +01:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -21,9 +21,12 @@ int main(void)
 
     for (int i=0; i<n; i++)
     {
-        for(int j=0; j<i+1; j++)
+         for(int k=n; k>0; k--)
         {
         printf(" ");
+        }
+        for(int j=0; j<i+1; j++)
+        {
         printf("#");
         }
         printf("\n");
