d19d95d4438d7670cb59806e31e745580658445b
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-06-14 21:53:47 +0000

    Wed, Jun 14, 2023, 10:53 PM +01:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -6,20 +6,25 @@ int main(void)
     // ask user for hopw many stairs
 
     int i=0;
+    int c=0;
 
 
-    while(i<1) | while(i>8))
+    while(i<1)
     {
         i = get_int("How many floors? ");
-
     }
 
-
-
+    while(i>8)
+    {
+        i = get_int("How many floors? ");
+    }
 
     //print on repeat removing one from number each time
 
-
-
+    for (c=0; c<i; c++)
+    {
+        printf("#");
+    }
+printf("\n");
 
 }
