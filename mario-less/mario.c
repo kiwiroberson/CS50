7d936fe65edea5a3c71e2597731b17fe6ed076fd
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-06-14 21:29:28 +0000

    Wed, Jun 14, 2023, 10:29 PM +01:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,7 +1,26 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    // ask user for hopw many stairs
+
+    int i=0;
+
+    while(i<1)
+    {
+        i = get_int("How many floors?");
+    }
+
+    //print on repeat removing one from number each time
+
+    while (i>1)
+    {
+        printf("#\n");
+        i = i-1;
+    }
+
+
+
+
 }
