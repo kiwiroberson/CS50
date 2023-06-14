6d382455bd102050ad6230ae496b18ffa9bd5456
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-06-14 14:26:53 +0000

    Wed, Jun 14, 2023, 3:26 PM +01:00

--- pset1/population.c
+++ pset1/population.c
@@ -7,21 +7,38 @@ int main (void)
 
 // ask for starting population end ending population
 
-    float i = get_int("Starting Population? ");
-    float j = get_int ("End Population? ");
+    int i = 0;
+    int j = 0;
     int c = 0;
     int n = 0;
- 
-//run loop while n < end population
 
-    while ( i < j)
+
+
+    //Get start and end population, refuse start until above 9 >
+
+
+    while (i < 9)
+    {
+        i = get_int("Starting Population? ");
+    }
+
+    while ( j < i)
+    {
+         j = get_int ("End Population? ");
+    }
+
+
+    n = i;
+
+    //run loop while n < end population
+    while ( n < j)
     {
        //loop for: n gets n + n/3 - n/4, then add one to counter
-        n = i/3;
+        n = (n + n/3 - n/4);
         c++;
     }
-    printf("Population: %f\n", n);
-    printf("Years: %f\n", c);
+    printf("Population: %i\n", n);
+    printf("Years: %i\n", c);
 
 //print  counter as output
 
