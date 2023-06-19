abb055267665f9e2889ebd5ca85d7bd9897c2997
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-06-19 22:04:38 +0000

    Mon, Jun 19, 2023, 11:04 PM +01:00

--- week2/test/scores.c
+++ week2/test/scores.c
@@ -2,17 +2,24 @@
 #include <cs50.h>
 
 float average(int array[]);
+const int N = 3;
+
 
 int main(void)
 {
-    int scores[3];
+    int scores[N];
 
-    for (int i = 0; i<3; i++)
+    for (int i = 0; i < N; i++)
     {
         scores[i] = get_int("Score: ");
     }
 
-    printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / (float) 3.0);
+    printf("Average: %f\n", average(scores);
 
 }
 
+float average (int array[])
+{
+ int sum = 0;
+ for int i = 0; i<N; i++
+}
