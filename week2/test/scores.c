878122c30aac5a92b5ef235c2a45eef8804f4c0e
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-06-19 21:49:27 +0000

    Mon, Jun 19, 2023, 10:49 PM +01:00

---
+++ week2/test/scores.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int score1 = 73;
+    int score2 = 72;
+    int score3 =33;
+
+    printf("Average: %f\n", (score1 +score2 +score3) / (float) 3.0);
+
+}
