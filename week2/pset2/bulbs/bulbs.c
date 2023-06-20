7c50b3e49e55c74952857b3c9e9caf86b30d8375
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-06-20 21:03:31 +0000

    Tue, Jun 20, 2023, 10:03 PM +01:00

---
+++ week2/pset2/bulbs/bulbs.c
@@ -0,0 +1,27 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+const int BITS_IN_BYTE = 8;
+
+void print_bulb(int bit);
+
+int main(void)
+{
+    string s = get_string("What is your message? );
+    
+}
+
+void print_bulb(int bit)
+{
+    if (bit == 0)
+    {
+        // Dark emoji
+        printf("\U000026AB");
+    }
+    else if (bit == 1)
+    {
+        // Light emoji
+        printf("\U0001F7E1");
+    }
+}
