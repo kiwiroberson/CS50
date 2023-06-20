b370d76e842b7c57cdff19659d961bbce73047b1
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-06-20 10:15:20 +0000

    Tue, Jun 20, 2023, 11:15 AM +01:00

--- week2/lab2/scrabble/scrabble.c
+++ week2/lab2/scrabble/scrabble.c
@@ -32,7 +32,7 @@ int compute_score(string word)
         {
            for (int i = 0, n = strlen(word); i < n; i++)
                 {
-                   
+                   int  POINTS[1];
                 }
 return printf("%c\n", word[0]);
         }
