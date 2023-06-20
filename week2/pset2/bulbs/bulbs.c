cf0ad84fb52d0e15bbd07419d15cb6868dfa1761
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-06-20 22:07:00 +0000

    Tue, Jun 20, 2023, 11:07 PM +01:00

--- week2/pset2/bulbs/bulbs.c
+++ week2/pset2/bulbs/bulbs.c
@@ -13,22 +13,25 @@ int main(void)
 
     for(int i = 0; i < n; i++)
     {
-        int j = s[i];
-        int a = j / 128;
-        j = j - a * 128;
-        int b = j / 64;
-        j = j - b * 64;
-        int c = j / 32;
-        j = j - c * 32;
-        int d = j / 16;
-        j = j - d * 16;
-        int e = j / 8;
-        j = j - e * 8;
-        int f = j / 4;
-        j = j - f * 4;
-        int g = j / 2;
-        j = j - g * 2;
-        int h = j;
+        for(int k = 7; k > 0; k--)
+        {
+            int j = s[i];
+            int a = j / 2^k;
+            j = j - a * 128;
+            int b = j / 64;
+            j = j - b * 64;
+            int c = j / 32;
+            j = j - c * 32;
+            int d = j / 16;
+            j = j - d * 16;
+            int e = j / 8;
+            j = j - e * 8;
+            int f = j / 4;
+            j = j - f * 4;
+            int g = j / 2;
+            j = j - g * 2;
+            int h = j;
+        }
 
         int z[8] = {a, b, c, d, e, f, g, h};
 
