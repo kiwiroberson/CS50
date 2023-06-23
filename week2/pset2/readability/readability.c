d20df354d00f679954cc8552c6eb60096e2c9d76
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-06-23 06:56:29 +0000

    Fri, Jun 23, 2023, 7:56 AM +01:00

--- week2/pset2/readability/readability.c
+++ week2/pset2/readability/readability.c
@@ -3,7 +3,12 @@
 
 int main(void)
 {
+    // Get sample Sentence, save as variable.
     string s = get_string("What text would you like to grade? ");
+
+    //Loop to count sentence length in characters
+
+    //Loop to count number of spaces in sentence.
 
     
 
