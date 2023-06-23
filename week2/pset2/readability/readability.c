a6368c3454817e5f44129e9c33e4dfea0054989e
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-06-23 07:01:36 +0000

    Fri, Jun 23, 2023, 8:01 AM +01:00

--- week2/pset2/readability/readability.c
+++ week2/pset2/readability/readability.c
@@ -4,13 +4,22 @@
 int main(void)
 {
     // Get sample Sentence, save as variable.
-    string s = get_string("What text would you like to grade? ");
+    string p = get_string("What text would you like to grade? ");
 
-    //Loop to count sentence length in characters
+    //Loop to count passage length in characters
 
-    //Loop to count number of spaces in sentence.
+    //Loop to count number of spaces in passage.
+    //Then calculate average word length.
+    //average number of letters per 100 words in the text
 
-    
+    //Loop to count number of ful stops in passage.
+    //Then calculate average sentence length.
+    //average number of sentences per 100 words in the text
+
+    //Calculation - index = 0.0588 * L - 0.296 * S - 15.8
+
+    printf("%i\n", p[1]);
+
 
 
 }
