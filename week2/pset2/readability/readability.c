7ba84dd243ca148ee6969e69085059a1fea5cfaf
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-06-24 20:21:04 +0000

    Sat, Jun 24, 2023, 9:21 PM +01:00

--- week2/pset2/readability/readability.c
+++ week2/pset2/readability/readability.c
@@ -35,11 +35,11 @@ int main(void)
 
 
     // NUMBER OF LETTERS PER 100 WORD
-    float numletters100word = ((i - numspace - numperiod)*100 / numspace);
+    float numletters100word = ((i - numspace - numperiod) / numspace) * 100;
 
 
     //number of sentences per 100 words
-    float numsentencesper100word = ((numperiod*100) / numspace);
+    float numsentencesper100word = ((numperiod) / numspace) * 100;
 
     float L = numletters100word;
     float S = numsentencesper100word;
