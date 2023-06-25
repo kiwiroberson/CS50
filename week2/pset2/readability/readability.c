812136dd275a74188cd69cd7a7b0b71bd101bbe8
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-06-25 17:27:45 +0000

    Sun, Jun 25, 2023, 6:27 PM +01:00

--- week2/pset2/readability/readability.c
+++ week2/pset2/readability/readability.c
@@ -12,65 +12,23 @@ int main(void)
     // Get sample Sentence, save as variable.
     string p = get_string("Text: ");
 
-    //Loop to count passage length in characters
-    int  i = 0;
-
-    //number of characters
-
-    float l = 0;
-
-    //NUMBER OF WORDS
-    float numspace = 1;
-
-    //NUMBER OF SENTENCES
-    float numperiod = 0;
-
+    //Functions
    int lettercount = count_letters(p);
    int sentencecount = count_sentences(p);
    int wordcount = count_words(p);
 
-
-
-
-    do
-    {
-        if (p[i] == 32)
-        {
-            numspace++;
-        }
-
-        if ((p[i] == 46)||(p[i] == 33)||(p[i] == 63))
-        {
-            numperiod++;
-        }
-
-        if (((p[i] >= 65) && (p[i] <= 90)) || ((p[i] >= 97) && (p[i] <= 122)))
-        {
-            l++;
-        }
-
-        i++;
-    }
-    while(p[i] != 0000);
-
-
     // NUMBER OF LETTERS PER 100 WORD
-    float numletters100word = (lettercount * 100 / numspace);
-
+    float L = (lettercount * 100 / wordcount);
 
     //number of sentences per 100 words
-    float numsentencesper100word = ((numperiod * 100) / numspace);
+    float S = ((sentencecount * 100) / wordcount);
 
-    float L = numletters100word;
-    float S = numsentencesper100word;
-
+    //Reading age calculaton
     float indexf = 0.0588 * L - 0.296 * S - 15.8;
-
     indexf = round(indexf);
     int index = indexf;
 
-     //printf("numletters100word: %i numsentencesper100word:%i\n", numletters100word, numsentencesper100word);
-     //printf("characters: %i, fullstops:%i, words:%i\n", i, numperiod, numspace);
+    //Print reading age
 
     if(index > 16)
     {
@@ -88,9 +46,9 @@ int main(void)
         }
     }
 
-     printf("Letters %i\n", lettercount);
-     printf("Words %i\n", wordcount);
-      printf("Sentence %i\n", sentencecount);
+    printf("Letters %i\n", lettercount);
+    printf("Words %i\n", wordcount);
+    printf("Sentence %i\n", sentencecount);
 }
 
 
@@ -138,25 +96,3 @@ int count_sentences(string p)
     }
     return g;
 }
-
-    //index = 0.0588 * L - 0.296 * S - 15.8;
-
-   // printf("Passage Length: %i Characters\n", i);
-    //printf("Space number: %i Characters\n", numspace);
-    //printf("period number: %i Characters\n", numperiod);
-    //printf("Index: %f \n", index);
-
-    //Loop to count number of spaces in passage.
-    //Then calculate average word length.
-    //average number of letters per 100 words in the text
-
-    //Loop to count number of ful stops in passage.
-    //Then calculate average sentence length.
-    //average number of sentences per 100 words in the text
-
-    //Calculation - index = 0.0588 * L - 0.296 * S - 15.8
-
-
-
-
-
