572b24037222d02ad346a1bd46288631a75ec4db
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-06-26 14:42:23 +0000

    Mon, Jun 26, 2023, 3:42 PM +01:00

--- week3/notes/notes1.c
+++ week3/notes/notes1.c
@@ -6,12 +6,12 @@ int main(void)
     int numbers[] = {20, 500, 10, 5, 100, 1, 50};
 
     int n = get_int("Number: ");
-    for (int = 0; i < 7; i++)
+    for (int i = 0; i < 7; i++)
     {
         if (numbers[i] == n)
         {
             printf("Found\n");
-            return 0
+            return 0;
         }
         printf("Not found\n");
         return 1;
