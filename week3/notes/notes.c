5edc1d2f06d804cdbe1f40f66e923907f6a74a2c
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-06-26 14:25:33 +0000

    Mon, Jun 26, 2023, 3:25 PM +01:00

---
+++ week3/notes/notes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <cs50.h>
+
+//Search algorithm
+
+//linear search
+//search each value from L-->R if found return treue
+bool Linear_search(doors)
+
+
+int main (void)
+{
+
+door[1] = get_int("int: ");
+bool Linear_search(doors);
+
+}
+
+
+
+bool Linear_search(doors)
+{
+
+    while (i < 5);
+    {
+        if (doors[i] == 50)
+        {
+            return true;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    return false;
+}
+
