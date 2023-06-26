29ad4b1e82814ca3479626b6c86f1acd25af1263
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-06-26 14:41:41 +0000

    Mon, Jun 26, 2023, 3:41 PM +01:00

--- week3/notes/notes1.c
+++ week3/notes/notes1.c
@@ -1,26 +1,20 @@
 #include <cs50.h>
-
-
+#include <stdio.h>
 
 int main(void)
 {
+    int numbers[] = {20, 500, 10, 5, 100, 1, 50};
 
-}
-
-int Linear_search(doors)
-{
-int i;
-    while (i < 5);
+    int n = get_int("Number: ");
+    for (int = 0; i < 7; i++)
     {
-        if (doors[i] == 50)
+        if (numbers[i] == n)
         {
-            return 1;
+            printf("Found\n");
+            return 0
         }
-        else
-        {
-            i++;
-        }
+        printf("Not found\n");
+        return 1;
     }
-    return 0;
 }
 
