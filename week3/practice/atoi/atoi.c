77ac5a5c8fbac72317552a3b4f0cd2a385c3ac9f
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-06-28 21:41:00 +0000

    Wed, Jun 28, 2023, 10:41 PM +01:00

--- week3/practice/atoi/atoi.c
+++ week3/practice/atoi/atoi.c
@@ -31,11 +31,10 @@ int convert(string input)
         return ((input[n - 1]) - 48);
     else
     {
-
-        input[n - 1] = 92;
+        return input[n - 1];
         input[n] = 0;
         convert(input);
-        return input[n - 1];
+
     }
 
 }
