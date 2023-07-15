51262811c367225647360aca81a121006e835be2
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-07-15 20:55:33 +0000

    Sat, Jul 15, 2023, 9:55 PM +01:00

---
+++ test.c
@@ -0,0 +1,17 @@
+#include cs50.h
+#include stdio.h
+#include string.h
+
+typedef struct
+{
+    string name;
+    int votes;
+}
+candidate;
+
+int main(void)
+{
+    candidate.names
+    string a = James
+
+}
