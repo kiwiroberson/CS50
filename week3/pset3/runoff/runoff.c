5e32cbac0eebb73717b5745c7369d1b3fbbdcf8e
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-07-18 23:33:47 +0000

    Wed, Jul 19, 2023, 12:33 AM +01:00

--- week3/pset3/runoff/runoff.c
+++ week3/pset3/runoff/runoff.c
@@ -150,13 +150,13 @@ void tabulate(void)
     {
         for(int j = 0; j < candidate_count; j++)
         {
-            if(candidates[j].eliminated == true)
+            if(candidates[j].eliminated == false)
             {
-                
+                candidates[preferences[i][j]].votes++;
             }
 
 
-                candidates[preferences[i][j]].votes++;
+
 
 
         }
