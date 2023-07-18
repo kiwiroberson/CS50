8bd83eca72310a27aaed55411aa59744fa58d15b
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-07-18 23:09:02 +0000

    Wed, Jul 19, 2023, 12:09 AM +01:00

--- week3/pset3/runoff/runoff.c
+++ week3/pset3/runoff/runoff.c
@@ -199,7 +199,15 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    // TODO
+    int minvoters = 0
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if ( candidates[i].votes == min)
+        {
+            minvoters++;
+        }
+    }
+    if(minvoters == (candidate_count))
     return false;
 }
 
