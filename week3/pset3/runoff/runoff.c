e9749f6206343a50b79306fe9707aa9e6863c4b0
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-07-18 23:14:33 +0000

    Wed, Jul 19, 2023, 12:14 AM +01:00

--- week3/pset3/runoff/runoff.c
+++ week3/pset3/runoff/runoff.c
@@ -217,6 +217,13 @@ bool is_tie(int min)
 // Eliminate the candidate (or candidates) in last place
 void eliminate(int min)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes == min)
+        {
+            candidates[i].eliminated = true;
+        }
+        candidates[i].votes = 0;
+    }
     return;
 }
