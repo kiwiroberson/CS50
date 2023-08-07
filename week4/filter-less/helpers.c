8eea8df8d28991fcf10c86e33503048fe5554738
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-07 16:19:52 +0000

    Mon, Aug 7, 2023, 5:19 PM +01:00

--- week4/filter-less/helpers.c
+++ week4/filter-less/helpers.c
@@ -89,7 +89,17 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    for(int i = 0; i < height; i++)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            int average = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
+            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtRed = average;
+
+        }
+    }
 
 
 
