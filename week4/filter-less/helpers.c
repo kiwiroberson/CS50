90b8a8a6ac8435a9846bf9ece9179b843e1d88bd
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-07 15:34:58 +0000

    Mon, Aug 7, 2023, 4:34 PM +01:00

--- week4/filter-less/helpers.c
+++ week4/filter-less/helpers.c
@@ -62,6 +62,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    
     return;
 }
 
