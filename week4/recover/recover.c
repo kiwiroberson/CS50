be84e7289ea50e211d1d28035499e3e95129b614
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-08 11:28:30 +0000

    Tue, Aug 8, 2023, 12:28 PM +01:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -25,9 +25,9 @@ int main(int argc, char *argv[])
 
     BYTE *buffer = malloc(512);
 
-    buffer[0] = 1;
+    buffer[0] = 0xff;
 
-    printf("%i\n", buffer[0])
+    printf("%i\n", buffer[0]);
 
     free(buffer);
 
