23ead089f4993007bb0f091e4fc39881c320d17b
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-08 13:37:47 +0000

    Tue, Aug 8, 2023, 2:37 PM +01:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -36,7 +36,6 @@ int main(int argc, char *argv[])
     if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
     {
 
-        fclose(img);
         //allocate space for filename
         char filename[8];
         //write new filenames to memory
