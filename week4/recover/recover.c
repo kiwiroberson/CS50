91e4ca445cd545657f0744f6006f28d8d1e937a6
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-08 11:35:46 +0000

    Tue, Aug 8, 2023, 12:35 PM +01:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -28,7 +28,10 @@ int main(int argc, char *argv[])
 
     while (fread(buffer, 1, 512, file) == 512)
     {
-    if()
+    if(buffer[0] != 0xff)
+    {
+        break;
+    }
 
     printf("%i\n", buffer[0]);
 
