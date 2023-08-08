c30a96dec26a32a53b5e1aca9800c956578c7080
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-08 11:59:11 +0000

    Tue, Aug 8, 2023, 12:59 PM +01:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
     //look for jpeg starting signature
     if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
     {
-
+        sprintf()
     printf("%i\n", buffer[0]);
     c++;
     }
