b8a84d55de5383431d60a3d1878c292eea64d988
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-08 11:23:14 +0000

    Tue, Aug 8, 2023, 12:23 PM +01:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -6,30 +6,24 @@ typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
-    if ( argc!)
+    if ( argc != 2)
+    {
+        printf("Useage: %s filename\n", argv[0]);
+    }
+
 
     //check type of entry valid
 
     //open memory card into memory with fopen
     FILE *file = fopen(argv[1], "r");
-    if (file !=NULL)
-    {
-
-    //repeat until end of card
-
-    BYTE *buffer[512] = malloc(512);
-    fread(*buffer, 1, 512, file);
-
-
-    printf("%i\n", buffer[0]);
-
-    free(buffer);
-    }
-
-    else
+    if (file == NULL)
     {
         printf("please enter valid filename\n");
         return 1;
     }
+
+
+
+
 return 0;
 }
