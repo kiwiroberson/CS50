f89f561b4153f2563718781f43e70d8527390fa1
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-08 10:08:47 +0000

    Tue, Aug 8, 2023, 11:08 AM +01:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -4,6 +4,8 @@
 int main(int argc, char *argv[])
 {
     //open memory card into memory with fopen
+    
+
 
     //repeat until end of card
 
@@ -17,19 +19,6 @@ int main(int argc, char *argv[])
 
     //close remaining files
 
-
-
-    //look for begining of JPEG
-    //fread
-    //look for: 0xff 0xd8 0xff 0xe0...0xef
-
-
-    //open a new jpeg file
-    //fopen
-    //fwrite
-    //write 512bytes until new jpeg found
-
-    //detect end of file with fread
 
 
 }
