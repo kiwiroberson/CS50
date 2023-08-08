d4dec62074334c00ffaa7f5dc52fd7fa91f8d153
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-08 13:05:30 +0000

    Tue, Aug 8, 2023, 2:05 PM +01:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -46,6 +46,7 @@ int main(int argc, char *argv[])
         fclose(img);
     //testing printing number of jpegs
     printf("%i\n", buffer[0]);
+    
     for (int i = 0; i < 512; i++)
             {
                 buffer[i] = 0;
