e001311821cdd6a60cf13e4a774112dc2f90a805
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-08 09:57:27 +0000

    Tue, Aug 8, 2023, 10:57 AM +01:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    //open memory card into memory
+    // fopen *f
+
+
+    //look for begining of JPEG
+    //look for: 0xff 0xd8 0xff 0xe0...0xef
+
+    //open a new jpeg file
+
+    //write 512bytes until new jpeg found
+
+
+
 }
