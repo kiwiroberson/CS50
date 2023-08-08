e2bd1a155a2fc203d796ac4fa6391df9bbd5938f
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-08 10:34:52 +0000

    Tue, Aug 8, 2023, 11:34 AM +01:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -20,9 +20,9 @@ int main(int argc, char *argv[])
 
 
     //repeat until end of card
-    while(fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
+    while(fread(buffer, 1, BLOCK_SIZE, *file) == BLOCK_SIZE)
     {
-        fread(buffer, 512, 1, )
+        fread(buffer, 512, 1, *file)
 
 
 
