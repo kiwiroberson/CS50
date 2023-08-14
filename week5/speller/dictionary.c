4a994912608c6327818a48c1a1351dc972b58e03
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-14 13:29:26 +0000

    Mon, Aug 14, 2023, 2:29 PM +01:00

--- week5/speller/dictionary.c
+++ week5/speller/dictionary.c
@@ -117,6 +117,9 @@ unsigned int size(void)
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
-    // TODO
+    for (i = 0, i < N; i++)
+    {
+        
+    }
     return false;
 }
