a64191ae7f70b592993dd69410aa35373e4057f7
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-14 10:17:26 +0000

    Mon, Aug 14, 2023, 11:17 AM +01:00

--- week5/inheritance/inheritance.c
+++ week5/inheritance/inheritance.c
@@ -112,11 +112,11 @@ void free_family(person *p)
     // TODO: Free parents recursively
     if (p->parents[0] != NULL)
     {
-        free_family(pparents[0]);
+        free_family(p->parents[0]);
     }
     if (p->parents[1] != NULL)
     {
-        free_family(parents[1]);
+        free_family(p->parents[1]);
     }
 
 
