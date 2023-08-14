5f7dba4db5ea6b79662eb3d1b08a07ca36d3246e
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-14 10:10:38 +0000

    Mon, Aug 14, 2023, 11:10 AM +01:00

--- week5/inheritance/inheritance.c
+++ week5/inheritance/inheritance.c
@@ -105,11 +105,18 @@ person *create_family(int generations)
 void free_family(person *p)
 {
     // TODO: Handle base case
-
+    if (p == NULL)
+    {
+        return
+    }
     // TODO: Free parents recursively
+
+    free(p->parent0);
+    free(p->parent1);
 
     // TODO: Free child
 
+    free(p);
 }
 
 // Print each family member and their alleles.
