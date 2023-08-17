e8002006c01293612d3b0c40a8e3a7c4d5d1f81d
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-17 15:35:09 +0000

    Thu, Aug 17, 2023, 4:35 PM +01:00

--- week6/sentimental-mario-less/mario.py
+++ week6/sentimental-mario-less/mario.py
@@ -1,3 +1,28 @@
 import CS50
 
-n = get_int()
+def main()
+    getnumber()
+    buildblock()
+
+
+
+
+
+
+
+
+
+def getnumber()
+    while true
+        n = CS50.get_int("What number? ")
+        if n > 1:
+            return n
+        elif n < 8:
+            return n
+
+def buildblock()
+    for x in range(n)
+        print(# * n)
+
+
+main()
