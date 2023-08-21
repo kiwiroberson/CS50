17ee687c8b017cd43d809d92501a1b415ff66e1a
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-21 17:28:26 +0000

    Mon, Aug 21, 2023, 6:28 PM +01:00

--- week6/sentimental-readability/sentimental-readability.py
+++ week6/sentimental-readability/sentimental-readability.py
@@ -1,24 +1,25 @@
 import cs50
 
 def main():
-    # Get text for analysis
-    text = cs50.get_string("Text: ")
+    ##get text for analysis
+    cs50.get_string("Text: ")
 
-    # Run analysis
+    ##Run analysis
     L = count_letters(text) / count_words(text) * 100
     S = count_sentences(text) / count_words(text) * 100
 
-    # Calculate score
-    CLi = 0.0588 * L - 0.296 * S - 15.8
-    CLi -= round(CLi)
+    ##calculate score
+    CLi = round(0.0588 * L - 0.296 * S - 15.8, 0)
+    CLi - round(CLi, 0)
 
-    # Print score
+    ##print score
     if CLi < 1:
-        print("Before Grade 1")
+        print("Before Grade 1" )
+        return
     elif CLi > 16:
         print("Grade 16+")
-    else:
-        print("Grade:", int(CLi))
+        return
+    print("Grade: " + str(int(CLi)))
 
 def count_letters(text):
     n = 0
@@ -27,12 +28,14 @@ def count_letters(text):
             n += 1
     return n
 
+
 def count_words(text):
     n = 1
     for a in text:
         if a.isspace():
             n += 1
     return n
+
 
 def count_sentences(text):
     n = 0
@@ -41,5 +44,5 @@ def count_sentences(text):
             n += 1
     return n
 
-if __name__ == "__main__":
-    main()
+
+main()
