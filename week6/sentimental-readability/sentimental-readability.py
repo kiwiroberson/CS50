3cdf14dab63d9ac47b4f88cbdeffc54c70badb65
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-21 07:41:18 +0000

    Mon, Aug 21, 2023, 8:41 AM +01:00

--- week6/sentimental-readability/sentimental-readability.py
+++ week6/sentimental-readability/sentimental-readability.py
@@ -2,13 +2,24 @@ import cs50
 import math
 
 def main():
+    ##get text for analysis
+    text = cs50.get_string("Text: ")
+
+    ##calculate score
+
+    ##round score
+
+    ##print score
+    print("Grade " x)
 
 
 def count_letters():
+    
+
 
 
 def count_words():
 
 
 def count_sentences():
-    
+
