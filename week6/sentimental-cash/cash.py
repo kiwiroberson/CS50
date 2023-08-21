7d951065c6aaf22fccc8e1b76a75a91af762f2ce
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-21 06:21:22 +0000

    Mon, Aug 21, 2023, 7:21 AM +01:00

--- week6/sentimental-cash/cash.py
+++ week6/sentimental-cash/cash.py
@@ -3,13 +3,13 @@ import cs50
 def main():
     change = cs50.get_float("Change owed: ")
 
-    q = change / 0.25
+    q = round(change / 0.25)
     qr = change % 0.25
 
-    d = qr / 0.1
+    d = round(qr / 0.1)
     dr = qr % 0.1
 
-    n = dr / 0.05
+    n = round(dr / 0.05)
     p = dr % 0.05
 
     print(q+d+n+p)
