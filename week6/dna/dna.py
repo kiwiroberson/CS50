61936c6f2ae35ad8775110df95c17012aa88cf70
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-21 22:23:57 +0000

    Mon, Aug 21, 2023, 11:23 PM +01:00

--- week6/dna/dna.py
+++ week6/dna/dna.py
@@ -20,8 +20,7 @@ def main():
 
 
     # TODO: Find longest match of each STR in DNA sequence
-    for column in reader:
-        print(longest_match(seq, reader))
+    print(reader)
     # TODO: Check database for matching profiles
 
     return
