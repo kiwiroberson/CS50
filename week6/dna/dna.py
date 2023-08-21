dc0b36f5b8080d290fcc10878481a3d4d64c827f
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-21 22:26:26 +0000

    Mon, Aug 21, 2023, 11:26 PM +01:00

--- week6/dna/dna.py
+++ week6/dna/dna.py
@@ -12,7 +12,7 @@ def main():
 
 
     # TODO: Read database file into a variable
-    with open(sys.argv[1]) as database:
+    with open(sys.argv[1], r, newline='') as database:
         reader = csv.DictReader(database)
     # TODO: Read DNA sequence file into a variable
     with open(sys.argv[2]) as sequence:
