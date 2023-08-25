90f5682141235a06e03d29d8e07ffed54a843511
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-25 05:19:25 +0000

    Fri, Aug 25, 2023, 6:19 AM +01:00

--- week6/sentimental-readability/readability.py
+++ week6/sentimental-readability/readability.py
@@ -1,1 +1,48 @@
-# TODO
+# TODOfrom cs50 import get_string
+
+def main():
+    #get text for analysis
+    text = get_string("Text: ")
+
+
+    #Run analysis
+    L = count_letters(text) / count_words(text) * 100
+    S = count_sentences(text) / count_words(text) * 100
+
+    #calculate score
+    CLi = round(0.0588 * L - 0.296 * S - 15.8, 0)
+
+    #print score
+    if CLi < 1:
+        print("Before Grade 1" )
+        return
+    elif CLi > 16:
+        print("Grade 16+")
+        return
+    print("Grade: " + str(int(CLi)))
+
+def count_letters(text):
+    n = 0
+    for a in text:
+        if a.isalpha():
+            n += 1
+    return n
+
+
+def count_words(text):
+    n = 1
+    for a in text:
+        if a.isspace():
+            n += 1
+    return n
+
+
+def count_sentences(text):
+    n = 0
+    for a in text:
+        if a == '.' or a == '?' or a == '!':
+            n += 1
+    return n
+
+
+main()
