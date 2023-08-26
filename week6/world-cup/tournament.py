e7d6bf63248500a0dd1eabcfe4b8b2db01f1c792
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-26 22:04:32 +0000

    Sat, Aug 26, 2023, 11:04 PM +01:00

--- week6/world-cup/tournament.py
+++ week6/world-cup/tournament.py
@@ -17,7 +17,10 @@ def main():
     teams = []
     # TODO: Read teams into memory from file
     with open(sys.argv[1]) as file:
-        reader = csv.DictReader(file)
+        reader = csv.reader(file)
+        for row in reader:
+            teams.append(row[0])
+    print(teams)
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
