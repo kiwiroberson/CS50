d1abf7c69f73a5f127500233fd6eff7cc3e4dafc
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-29 15:17:25 +0000

    Tue, Aug 29, 2023, 4:17 PM +01:00

--- week7/prophecy/insertdata.py
+++ week7/prophecy/insertdata.py
@@ -4,18 +4,21 @@ import csv
 #open database
 db = SQL("sqlite:///roster.db")
 houses = []
+heads = []
 #open students.csv
 with open("students.csv", "r") as file:
     reader =  csv.DictReader(file)
     for row in reader:
         if row["house"] not in houses:
             houses.append(row["house"])
+        if row["head"] not in heads:
+            heads.append(row["head"])
 
     for row in reader
         #insert id and name into students table1 via SQLite3
         db.execute("INSERT INTO students (id, student_name) VALUES (?, ?)", row["id"], row["student_name"])
-# #db.execute("INSERT INTO students (id, student_name) VALUES (?, ?)", row["id"], row["student_name"])
-
-
-#insert house name and head into table2
-#insert house allocation information into table3
+        #insert house name and head into table2
+        for i in range(4):
+            db.execute("INSERT INTO house (id, house) VALUES (?, ?)", houses[i], heads[i])
+        #insert house allocation information into table3
+        
