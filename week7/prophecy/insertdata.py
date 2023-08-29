614f9cfdd24d75eceb3fba785c4a9dbbd8353423
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-29 14:13:43 +0000

    Tue, Aug 29, 2023, 3:13 PM +01:00

--- week7/prophecy/insertdata.py
+++ week7/prophecy/insertdata.py
@@ -4,4 +4,5 @@ import csv
 with open("students.csv", "r") as file:
     reader =  csv.DictReader(file)
     for row in reader:
-        print(row["student_name"])
+        print(row["id"])
+        
