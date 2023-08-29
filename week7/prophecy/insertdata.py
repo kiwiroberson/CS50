53545ababd3de08197d0a0681e2aa3b08bf308ec
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-29 14:17:21 +0000

    Tue, Aug 29, 2023, 3:17 PM +01:00

--- week7/prophecy/insertdata.py
+++ week7/prophecy/insertdata.py
@@ -1,8 +1,17 @@
 from cs50 import SQL
 import csv
 
+#open database
+db = SQL("sqlite:///roster.db")
+
+#open students.csv
 with open("students.csv", "r") as file:
     reader =  csv.DictReader(file)
+    #iterate over rows
     for row in reader:
         print(row["id"])
-        
+    #insert id and name into students table1 via SQLite3
+
+    #insert house name and head into table2
+
+    #insert house allocation information into table3
