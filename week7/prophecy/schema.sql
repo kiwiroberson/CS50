3d0109b22e32f00fbdf29a245367fb4eaae7b128
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-29 13:48:31 +0000

    Tue, Aug 29, 2023, 2:48 PM +01:00

---
+++ week7/prophecy/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE students (
+    id INTEGER,
+    student_name TEXT,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE house (
+
+)
+
+CREATE TABLE link (
+    student_id INTEGER,
+    house ID
+)
