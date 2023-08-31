73b00890fc9673aa21384073ba279e42444934dd
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-31 12:39:50 +0000

    Thu, Aug 31, 2023, 1:39 PM +01:00

--- week7/fiftyville/test.sql
+++ week7/fiftyville/test.sql
@@ -1,7 +1,8 @@
-SELECT bakery_security_logs.id, hour, minute, activity, bakery_security_logs.license_plate, name
-FROM bakery_security_logs
-JOIN people ON bakery_security_logs.license_plate = people.license_plate
+SELECT *
+FROM atm_transactions
+JOIN bank_accounts ON atm_transactions.account_number = bank_accounts.account_number
+JOIN people ON bank_accounts.person_id = people.id
 WHERE year = "2021"
 AND month = "7"
 AND day = "28"
-
+AND atm_location = "Leggett Street"
