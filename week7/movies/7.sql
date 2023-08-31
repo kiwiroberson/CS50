89611a28f675365a953b908776f512de470891f2
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-31 08:51:35 +0000

    Thu, Aug 31, 2023, 9:51 AM +01:00

--- week7/movies/7.sql
+++ week7/movies/7.sql
@@ -1,7 +1,6 @@
 SELECT title, rating from movies
 JOIN ratings ON movies.id = ratings.movie_id
-WHERE year = "2010"
-    AND rating = "NULL"
+WHERE rating IS null
 ORDER BY rating DESC, title
 LIMIT 10
 
