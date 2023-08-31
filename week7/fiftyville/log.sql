9c4d3ca08445447da80537cda2672116f065c373
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-31 12:04:40 +0000

    Thu, Aug 31, 2023, 1:04 PM +01:00

--- week7/fiftyville/log.sql
+++ week7/fiftyville/log.sql
@@ -10,7 +10,12 @@ AND street  = "Humphrey Street"
 
 -- reports theft of duck at 10:15am, three witnesses all interview transcripts mention bakery
 --query interview database
-    -- avenue #1: left car park in 10 mins after theft
+    -- avenue #1: left car park in 10 mins after theft - security footage
 
-    -- avenue 2: 
+    --avenue #2: In morning theif withdrawing money at ATM in Legett street
+
+    -- avenue #3: as leaving bakery theif called someone for less than a minute,
+    --              planned to take earliest outward flight on 29/7/21 - accomplice  person buying ticket
+
+
 
