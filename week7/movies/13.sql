75f80b57e72e161bc8b94d8e2a6e628d56b615e9
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-31 09:30:40 +0000

    Thu, Aug 31, 2023, 10:30 AM +01:00

--- week7/movies/13.sql
+++ week7/movies/13.sql
@@ -2,7 +2,8 @@ SELECT name
 FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people on stars.person_id = people.id
-WHERE name IN (SELECT DISTINCT title
+WHERE name IN
+    (SELECT name
      FROM movies
      JOIN stars ON movies.id = stars.movie_id
      JOIN people on stars.person_id = people.id
