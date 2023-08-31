94c81d23d65ed5ea2d31e60280fee9a273d279f7
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-31 08:35:59 +0000

    Thu, Aug 31, 2023, 9:35 AM +01:00

--- week7/songs/8.sql
+++ week7/songs/8.sql
@@ -1,1 +1,3 @@
-SELECT songs.name FROM songs JOIN artists on songs.artist_id = artists.id WHERE songs.name LIKE "%feat.%";
+SELECT songs.name FROM songs
+JOIN artists on songs.artist_id = artists.id
+WHERE songs.name LIKE "%feat.%";
