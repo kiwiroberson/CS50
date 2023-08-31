effe1605558de5366b51c1ce2924be54a528a32b
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-31 09:25:14 +0000

    Thu, Aug 31, 2023, 10:25 AM +01:00

--- week7/movies/12.sql
+++ week7/movies/12.sql
@@ -3,5 +3,7 @@ FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people on stars.person_id = people.id
 WHERE people.name = "Bradley Cooper"
-FROM movies
-WHERE people.name = "Jennifer Lawrence"
+AND title IN
+    (SELECT DISTINCT title
+     FROM movies
+     people.name = "Bradley Cooper")
