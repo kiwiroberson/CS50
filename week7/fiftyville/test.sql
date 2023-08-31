988c9edde6efabfa1907c2ac59e009f8fbace51b
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-31 12:55:55 +0000

    Thu, Aug 31, 2023, 1:55 PM +01:00

--- week7/fiftyville/test.sql
+++ week7/fiftyville/test.sql
@@ -6,14 +6,37 @@ WHERE year = "2021"
 AND month = "7"
 AND day = "29"
 
-WHERE name
-FROM
+AND name IN
 (
+SELECT name
+FROM people
 JOIN phone_calls ON  people.phone_number = phone_calls.caller
 WHERE year = "2021"
 AND month = "7"
 AND day = "28"
 AND duration < 60
+)
+
+AND name IN
+(
+SELECT name
+FROM atm_transactions
+JOIN bank_accounts ON atm_transactions.account_number = bank_accounts.account_number
+JOIN people ON bank_accounts.person_id = people.id
+WHERE year = "2021"
+AND month = "7"
+AND day = "28"
+AND atm_location = "Leggett Street"
+)
+
+AND name IN
+(
+SELECT name
+FROM bakery_security_logs
+JOIN people ON bakery_security_logs.license_plate = people.license_plate
+WHERE year = "2021"
+AND month = "7"
+AND day = "28"
 )
 
 
