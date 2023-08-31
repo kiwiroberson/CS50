8c7af4e52a92f97df7b1f771f75b57b0029b7b8f
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-31 12:33:23 +0000

    Thu, Aug 31, 2023, 1:33 PM +01:00

--- week7/fiftyville/log.sql
+++ week7/fiftyville/log.sql
@@ -17,15 +17,20 @@ AND month = "7"
 AND day = "28"
 
     -- avenue #1: left car park in 10 mins after theft - security footage
-           -- | 259 | 10   | 14     | entrance | 13FNH73       |
-           -- | 260 | 10   | 16     | exit     | 5P2BI95       |
-           -- | 261 | 10   | 18     | exit     | 94KL13X       |
-         --   | 262 | 10   | 18     | exit     | 6P58WS2       |
-          --  | 263 | 10   | 19     | exit     | 4328GD8       |
-          --  | 264 | 10   | 20     | exit     | G412CB7       |
-          --  | 265 | 10   | 21     | exit     | L93JTIZ       |
-           -- | 266 | 10   | 23     | exit     | 322W7JE       |
-          --  | 267 | 10   | 23     | exit     | 0NTHK55       |
+SELECT bakery_security_logs.id, hour, minute, activity, bakery_security_logs.license_plate, name
+FROM bakery_security_logs
+JOIN people ON bakery_security_logs.license_plate = people.license_plate
+WHERE year = "2021"
+AND month = "7"
+AND day = "28"
+         --   | 260 | 10   | 16     | exit     | 5P2BI95       | Vanessa |
+        --    | 261 | 10   | 18     | exit     | 94KL13X       | Bruce   |
+          --  | 262 | 10   | 18     | exit     | 6P58WS2       | Barry   |
+         --   | 263 | 10   | 19     | exit     | 4328GD8       | Luca    |
+         --   | 264 | 10   | 20     | exit     | G412CB7       | Sofia   |
+         --   | 265 | 10   | 21     | exit     | L93JTIZ       | Iman    |
+          --  | 266 | 10   | 23     | exit     | 322W7JE       | Diana   |
+         --   | 267 | 10   | 23     | exit     | 0NTHK55       | Kelsey  |
 
 
     --avenue #2: In morning theif withdrawing money at ATM in Legett street
