7f1832feadab903c812364630d1ec740251ddc1e
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-31 12:51:42 +0000

    Thu, Aug 31, 2023, 1:51 PM +01:00

--- week7/fiftyville/test.sql
+++ week7/fiftyville/test.sql
@@ -1,3 +1,9 @@
 SELECT name
 FROM people
 JOIN passengers ON  people.passport_number = passengers.passport_number
+JOIN flights ON passengers.flight_id = flights.id
+WHERE year = "2021"
+AND month = "7"
+AND day = "29"
+
+
