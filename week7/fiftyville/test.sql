067881933c09a80438531599ae017c8ddf582e55
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-31 12:31:46 +0000

    Thu, Aug 31, 2023, 1:31 PM +01:00

--- week7/fiftyville/test.sql
+++ week7/fiftyville/test.sql
@@ -1,6 +1,6 @@
-SELECT id, hour, minute, activity, license_plate
+SELECT bakery_security_logs.id, hour, minute, activity, bakery_security_logs.license_plate, name
 FROM bakery_security_logs
-JOIN people ON 
+JOIN people ON bakery_security_logs.license_plate = people.license_plate
 WHERE year = "2021"
 AND month = "7"
 AND day = "28"
