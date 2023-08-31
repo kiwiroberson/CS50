25628bc7cde7bbd5b7bcf5ff691f77eab40bf819
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-31 12:49:59 +0000

    Thu, Aug 31, 2023, 1:49 PM +01:00

--- week7/fiftyville/test.sql
+++ week7/fiftyville/test.sql
@@ -1,7 +1,3 @@
 SELECT name
 FROM people
-JOIN flights ON  people.pho = phone_calls.caller
-WHERE year = "2021"
-AND month = "7"
-AND day = "28"
-AND duration < 60
+JOIN passengers ON  people.passport_number = passengers.passport_number
