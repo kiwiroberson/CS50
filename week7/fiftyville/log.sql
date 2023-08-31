d65e5abd2f63b6376a942c626b68672dcc687e58
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-31 12:28:13 +0000

    Thu, Aug 31, 2023, 1:28 PM +01:00

--- week7/fiftyville/log.sql
+++ week7/fiftyville/log.sql
@@ -17,7 +17,16 @@ AND month = "7"
 AND day = "28"
 
     -- avenue #1: left car park in 10 mins after theft - security footage
-    
+           -- | 259 | 10   | 14     | entrance | 13FNH73       |
+           -- | 260 | 10   | 16     | exit     | 5P2BI95       |
+           -- | 261 | 10   | 18     | exit     | 94KL13X       |
+         --   | 262 | 10   | 18     | exit     | 6P58WS2       |
+          --  | 263 | 10   | 19     | exit     | 4328GD8       |
+          --  | 264 | 10   | 20     | exit     | G412CB7       |
+          --  | 265 | 10   | 21     | exit     | L93JTIZ       |
+           -- | 266 | 10   | 23     | exit     | 322W7JE       |
+          --  | 267 | 10   | 23     | exit     | 0NTHK55       |
+
 
     --avenue #2: In morning theif withdrawing money at ATM in Legett street
 
