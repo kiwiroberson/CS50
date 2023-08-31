6f090a57b287942f6d8cf358d361d5794460c881
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-08-31 09:50:41 +0000

    Thu, Aug 31, 2023, 10:50 AM +01:00

--- week7/fiftyville/log.sql
+++ week7/fiftyville/log.sql
@@ -1,2 +1,13 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 --  July 28, 2021 and that it took place on Humphrey Street.
+-- Query description from crime_scene_reports
+SELECT description
+FROM crime_scene_reports
+WHERE year = "2021"
+AND month = "7"
+AND day = "28"
+AND street  = "Humphrey Street"
+
+-- reports theft of duck at 10:15am, three witnesses all interview transcripts mention bakery
+--query interview database
+
---
+++ week7/fiftyville/test.sql
@@ -0,0 +1,5 @@
+SELECT id, name, transcript
+FROM interviews
+WHERE year = "2021"
+AND month = "7"
+AND day = "28"
