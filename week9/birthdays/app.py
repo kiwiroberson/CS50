0fa12f36a447eef04ead5e59dcd9866e23d52548
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-09-12 16:55:18 +0000

    Tue, Sep 12, 2023, 5:55 PM +01:00

--- week9/birthdays/app.py
+++ week9/birthdays/app.py
@@ -12,6 +12,8 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///birthdays.db")
 
+PEOPLE = 
+
 
 @app.after_request
 def after_request(response):
