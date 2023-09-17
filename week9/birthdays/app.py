be2a94fb29e8795fe2498732b0e5f6a7b5676186
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-09-17 20:23:42 +0000

    Sun, Sep 17, 2023, 9:23 PM +01:00

--- week9/birthdays/app.py
+++ week9/birthdays/app.py
@@ -27,11 +27,11 @@ def index():
         name = request.form.get("name")
         day = request.form.get("day")
         month = request.form.get("month")
-        x = request.form.get("id")
+        n = request.form.get("id")
         #remember submitted birthday
         db.execute("INSERT INTO birthdays (name, day, month) VALUES(?, ?, ?)", name, day, month)
         #delete selected record
-        db.execute("DELETE FROM birthdays WHERE id=?", x)
+        db.execute("DELETE FROM birthdays WHERE id=7")
         return redirect("/")
 
     else:
