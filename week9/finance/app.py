edc492d0d34ad2a7604062a3f074d5e6baf5055f
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-09-18 22:38:15 +0000

    Mon, Sep 18, 2023, 11:38 PM +01:00

--- week9/finance/app.py
+++ week9/finance/app.py
@@ -109,7 +109,7 @@ def quote():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-      if request.method == "POST":
+    if request.method == "POST":
 
         # Ensure username was submitted
         if not request.form.get("username"):
