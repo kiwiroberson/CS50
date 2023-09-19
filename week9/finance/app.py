26cdafb98818360e0543c5d1f932f9d37bad6e7c
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-09-19 14:14:17 +0000

    Tue, Sep 19, 2023, 3:14 PM +01:00

--- week9/finance/app.py
+++ week9/finance/app.py
@@ -4,6 +4,7 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
 
 from helpers import apology, login_required, lookup, usd
 
@@ -54,10 +55,12 @@ def buy():
             return apology("Shares not positive", "703")
         print(session)
         userid = session
-        stock = lookup(request.form.get("symbol"))
-        shares=1
-        price=1
-        date=1
+        stockdetails = lookup(request.form.get("symbol"))
+        stock = stockdetails["symbol"]
+        print(stock)
+        shares = int(request.form.get("shares"))
+        price = stockdetails["price"]
+        date = date.today()
         return redirect("/")
 
 
