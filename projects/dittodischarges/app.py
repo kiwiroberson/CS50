48f487591d705f7c602f7fbc92fd1d090168d75d
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-09-21 17:23:50 +0000

    Thu, Sep 21, 2023, 6:23 PM +01:00

--- projects/dittodischarges/app.py
+++ projects/dittodischarges/app.py
@@ -5,4 +5,16 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from
+from helpers import apology, login_required
+
+#configure application
+app =  Flask(_name_)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///discharge.db")
+
