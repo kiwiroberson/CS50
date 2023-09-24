a6d3ad289bdc05c31866fee8385b53f00b4a1110
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-09-24 16:52:05 +0000

    Sun, Sep 24, 2023, 5:52 PM +01:00

--- projects/dittodischarges/app.py
+++ projects/dittodischarges/app.py
@@ -8,7 +8,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from helpers import apology, login_required
 
 #configure application
-app =  Flask(_name_)
+app =  Flask(__name__)
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
