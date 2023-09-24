525dda80b3cb87d08434700d2b74f23309877b0e
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-09-24 16:47:56 +0000

    Sun, Sep 24, 2023, 5:47 PM +01:00

--- projects/dittodischarges/app.py
+++ projects/dittodischarges/app.py
@@ -16,7 +16,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-##########################db = SQL("sqlite:///discharge.db")#############################
+db = SQL("sqlite:///discharge.db")
 
 @app.after_request
 def after_request(response):
