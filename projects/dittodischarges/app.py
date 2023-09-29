e4768aa973eb817765aa0104b30508b0fed318e1
kiwiroberson <112025594+kiwiroberson@users.noreply.github.com>
2023-09-29 07:00:39 +0000

    Fri, Sep 29, 2023, 8:00 AM +01:00

--- projects/dittodischarges/app.py
+++ projects/dittodischarges/app.py
@@ -19,7 +19,7 @@ Session(app)
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///discharge.db")
 
-diagnosistable={}
+diagnosistable=[]
 db.execute("SELECT * FROM diagnosis;")
 
 
